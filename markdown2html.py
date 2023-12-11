1a87eed5e59632d5c98cd28c7a2e29182833ffd9
SimonDesc <pro.descombes@gmail.com>
2023-12-11 10:57:21 +0100

    fix: documentation

--- markdown2html.py
+++ markdown2html.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+"""
+Ce script est un outil de ligne de commande pour convertir un
+fichier Markdown en fichier HTML.
+Il prend deux arguments : le fichier source en Markdown et
+le nom du fichier de sortie HTML.
+Le script vérifie la validité des arguments et l'existence du fichier source
+avant de procéder à la conversion.
+"""
 import sys
 import os
 
-# Ce script est un outil de ligne de commande pour convertir un
-# fichier Markdown en fichier HTML.
-# Il prend deux arguments : le fichier source en Markdown et
-# le nom du fichier de sortie HTML.
-# Le script vérifie la validité des arguments et l'existence du fichier source
-# avant de procéder à la conversion.
+
 if __name__ == "__main__":
     if (sys.argv is False or len(sys.argv) < 3 or len(sys.argv) > 3):
         sys.stderr.write('Usage: ./markdown2html.py README.md README.html \n')
